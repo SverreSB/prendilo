0765d7771a6529e3a2b9c375da7203ce54e7be2b
SverreSB <sverre.sb@gmail.com>
2019-02-07 08:33:10 -0800

    Is now deleting the food after a transaction. I have to refactor this so I am redirecting to delte food instead, but I have to figure out how to redirect to delete request first

--- api/routes/transaction/transaction.js
+++ api/routes/transaction/transaction.js
@@ -1,6 +1,7 @@
 const {User} = require('../../../models/objects/users/user');
 const{Food} = require('../../../models/objects/food/food');
 const auth = require('../../../middleware/auth');
+const asyncMiddleware = require('../../../middleware/async');
 const validation = require('../../helpers/transaction/validation');
 const express = require('express');
 const router = express.Router();
@@ -12,7 +13,7 @@ const router = express.Router();
         Checks if receiver has enough foodStamps, if true then giver gets
         0.5 towards earned stamps. Receiver decrements -1 foodStamps. 
  */
-router.post('/', auth, async (req,res) => {
+router.post('/', auth, asyncMiddleware (async (req,res) => {
     const validate = validation(req.body);
 
     if(validate.error) return res.status(400).send(validate.error.details[0].message);
@@ -21,7 +22,7 @@ router.post('/', auth, async (req,res) => {
     const food = await Food.findById(req.body._id);
 
     if(!receiver) return res.status(400).send('Receiver not found');
-    if(!food) return res.status(400).send("Food boject not found");
+    if(!food) return res.status(400).send("Food object not found");
 
     if(!enoughStamps(receiver)) return res.status(400).send('Insufficient funds');
     
@@ -33,9 +34,10 @@ router.post('/', auth, async (req,res) => {
 
     receiver.foodStamp -= 1;
     receiver.save();
-
-    res.send(receiver); //maybe send a req for api/update/delete?
-});
+    food.delete();
+    res.send(receiver);//maybe send a req for api/update/delete?
+    /*.redirect(`/api/update/delete/${food._id}`)*/ 
+}));
 
 /**
  *  Boolean function that checks if user has foodStamps or not 
