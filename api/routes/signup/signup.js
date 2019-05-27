6f2b9a4cd0735a6300c6a058998f5e159280d52c
SverreSB <sverre.sb@gmail.com>
2019-05-27 21:42:38 +0200

    Added email validation on signup

--- api/routes/signup/signup.js
+++ api/routes/signup/signup.js
@@ -24,7 +24,7 @@ const {User, validateSignup} = require('../../../models/objects/users/user');
         The password is salted and hashed before being stored in db
  */
 router.post('/', asyncMiddleware( async(req, res) => {
-    
+    if(!validateEmail(req.body.email)) return res.status(400).send('Invalid email');
     req.body.lat = generateLat();
     req.body.long = generateLong();
     req.body.foodStamp = 5;
@@ -58,4 +58,9 @@ function generateLong(){
     const min = -121.751907;
     const max = -121.816795;
     return (Math.random() * (max - min) + min).toFixed(6); 
+}
+
+function validateEmail(email) {
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
 }
