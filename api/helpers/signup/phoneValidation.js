5cb77768ec2bcd0cdfa43cd7404c2a4bcdd1196b
SverreSB <sverre.sb@gmail.com>
2019-06-05 14:03:27 +0200

    Added comments

--- api/helpers/signup/phoneValidation.js
+++ api/helpers/signup/phoneValidation.js
@@ -5,19 +5,26 @@ const nexmo = new Nexmo({
   apiSecret: getAPISecret()
 })
 
-
+/**
+ *  Function for generating and sending out a validtion code to phone number
+        Takes in phoneNumber as parameter
+        Turning phone number to string, generating code to concatinate with text
+        Sending out sms to given phonenumber from prendilo with text
+        Returning code so it can be stored in the database for validation
+ */
 module.exports = function(phoneNumber) {
     const from = 'Prendilo';
     const to = '' + phoneNumber;
     var text = 'Your validation code: ';
     const code = generateValidation();
-    
     text += code;
+
     nexmo.message.sendSms(from, to, text)
     
     return code;
 }
 
+//Function for generating a random 6 figure code
 function generateValidation() {
     return Math.floor((Math.random() * 999999) + 100000);
 }
