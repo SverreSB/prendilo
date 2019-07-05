d86639daf45f72ae9cf53da868950b837d9264e5
SverreSB <sverre.sb@gmail.com>
2019-07-05 09:27:36 +0200

    added validation for participants length in chat

--- api/routes/chat/start.js
+++ api/routes/chat/start.js
@@ -5,16 +5,19 @@ const _ = require('lodash');
 
 const asyncMiddleware = require('../../../middleware/async');
 const {User} = require('../../../models/objects/users/user');
-const {Chat} = require('../../../models/objects/chat/chat');
+const {Chat, validateChat} = require('../../../models/objects/chat/chat');
 
 
 
 router.post('/', asyncMiddleware( async(req, res) =>{
     const giver = await User.findOne({phone: req.body.giver});
     const receiver = await User.findById(req.body.receiver);
-    req.body.participants = [giver._id, receiver._id]
-    req.body.messages = [{"sender": giver.name, "message": req.body.message}]
-    const chat = new Chat(_.pick(req.body, ['participants', 'messages']))
+    req.body.participants = [giver._id, receiver._id, "aff"]
+    req.body.messages = [{"sender": giver.name, "message": req.body.message}];
+    const chat = new Chat(_.pick(req.body, ['participants', 'messages']));
+
+    /*const validateInput = validateChat(req.body);
+    if(validateInput.error) return res.status(400).send(validateInput.error.details[0].message)*/
     chat.save();
     res.status(200).send('test');
 }))
--- models/objects/chat/chat.js
+++ models/objects/chat/chat.js
@@ -5,13 +5,30 @@ const {ChatMessageSchema} = require('../../schema/chatMessages');
 const schema = new mongoose.Schema({
     participants: {
         type: [String],
-        required: true
+        required: true,
+        validate: [arrayLimit, '{PATH} must contain 2 items(latitude and longitude)']
     },
     messages: {
         type: [ChatMessageSchema]
     }
 });
 
+function arrayLimit(val){ 
+    return val.length == 2;
+}
+
 const Chat = mongoose.model('Chat', schema);
 
+function validateChat(body) {
+    const schema = {
+        participants: Joi.array().required(),
+        messages: Joi.array().required()
+    }
+
+    const validation = Joi.validate(body, schema);
+
+    return validation;
+}
+
 exports.Chat = Chat
+exports.validateChat = validateChat
