bf6a53c93ea6383d102fcff0ddc1d29b0f1dc586
SverreSB <sverre.sb@gmail.com>
2019-07-22 10:02:10 +0200

    Deleted unused helper files in models, those were refactored to helpers in api

--- models/helpers/saltAndHash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function saltAndHash(key) {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(key, salt);
-    return hash;
-}
-
-exports.saltAndHash = saltAndHash;
